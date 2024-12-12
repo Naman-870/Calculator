d8cf7df0eea7e66759442dfdefbdf7b87a641473
Naman Chaudhary <namanchaudhary0027@gmail.com>
2024-12-12 19:06:55 +0530

    Intial working code

---
+++ Calculator.java
@@ -0,0 +1,50 @@
+// Calculator.java
+import java.util.Scanner;
+
+public class Calculator {
+    public void start() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Welcome to the Simple Calculator!");
+        System.out.println("Choose an operation: +, -, *, /");
+        char operation = scanner.next().charAt(0);
+
+        System.out.println("Enter the first number:");
+        double num1 = scanner.nextDouble();
+
+        System.out.println("Enter the second number:");
+        double num2 = scanner.nextDouble();
+
+        double result = 0;
+        boolean valid = true;
+
+        switch (operation) {
+            case '+':
+                result = num1 + num2;
+                break;
+            case '-':
+                result = num1 - num2;
+                break;
+            case '*':
+                result = num1 * num2;
+                break;
+            case '/':
+                if (num2 != 0) {
+                    result = num1 / num2;
+                } else {
+                    System.out.println("Error: Division by zero is not allowed.");
+                    valid = false;
+                }
+                break;
+            default:
+                System.out.println("Error: Invalid operation.");
+                valid = false;
+        }
+
+        if (valid) {
+            System.out.println("Result: " + result);
+        }
+
+        scanner.close();
+    }
+}
---
+++ Main.java
@@ -0,0 +1,7 @@
+// Main.java
+public class Main {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        calculator.start();
+    }
+}
